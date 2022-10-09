2b00e467ed4c6ec44d943c7a5649ea7ee613af86
JonatSrG <JonatSrG>
2022-10-09 13:31:44 -0500

    Offline basica

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
 
 
+self.addEventListener('fetch', event => {
 
+    const offlineResp = new Response(`
+        Bienvenido a mi Pagina Web
+
+        Disculpa, pero para usarla, necesitas internet!!
+    `);
+    
+    const resp = fetch(event.request)
+        .catch( () => offlineResp );
+        
+    event.respondWith( resp );
+});
